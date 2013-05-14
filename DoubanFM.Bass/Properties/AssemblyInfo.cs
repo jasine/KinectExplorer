81aecb2b222c1a291ec66d3c88c9512e9c30e46b
jasine <jasinechen@gmail.com>
2013-05-14 13:55:13 +0800

    small fix

--- DoubanFM.Bass/Properties/AssemblyInfo.cs
+++ DoubanFM.Bass/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("DoubanFM.Bass")]
+[assembly: AssemblyTitle("GesturesRecognize")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DoubanFM.Bass")]
+[assembly: AssemblyProduct("KinectExplorer")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
